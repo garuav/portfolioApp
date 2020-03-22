4c78f9f57c82d5f90b4bd4183d532fcb9c2a51fa
garuav <gbgaurav461@gmail.com>
2020-03-22 18:55:04 +0530

    added code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Firebase } from '@ionic-native/firebase/ngx';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 
 @Component({
   selector: 'app-root',
@@ -26,7 +28,9 @@ export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private firebase: Firebase,
+    private faio: FingerprintAIO
   ) {
     this.initializeApp();
   }
@@ -35,6 +39,8 @@ export class AppComponent implements OnInit {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.initFirebase();
+      this.initFingerprint();
     });
   }
 
@@ -45,5 +51,32 @@ export class AppComponent implements OnInit {
         page => page.title.toLowerCase() === path.toLowerCase()
       );
     }
+  }
+
+  initFirebase() {
+    this.firebase
+      .getToken()
+      .then(token => {
+        console.log(`The token is ${token}`);
+        localStorage.setItem('token', token);
+      }) // save the token server-side and use it to push notifications to this device
+      .catch(error => console.error('Error getting token', error));
+
+    this.firebase
+      .onNotificationOpen()
+      .subscribe(data => console.log(`User opened a notification ${data}`));
+
+    this.firebase
+      .onTokenRefresh()
+      .subscribe((token: string) => console.log(`Got a new token ${token}`));
+  }
+  initFingerprint() {
+    this.faio
+      .show({
+        disableBackup: true,
+        description: 'Some biometric description',
+      })
+      .then((result: any) => console.log(result))
+      .catch((error: any) => console.log(error));
   }
 }
